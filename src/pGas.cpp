e26ce58376dead713f3d16a2e53e7154ab4fda20
punkundermyskin <p.nkundermyskin@gmail.com>
2019-12-01 23:44:29 +0300

    updated split/combine cubes logic
    based on the idea of extended cube, a brand new method for splitting and combining parts of a cube was implemented. Now from the basic cube (ex. 6x6x6) we extract the internal pieces (x8 - 2x2x2) as a variable parts and other external pieces as a constants. We send to the nodes cubes that have both - external and internal part (x8 - 4x4x4 -node's cube). Each nodes (processes) updates the values of internal part node's cube then extract internal part as a separate cube and send it to the master.
    Master combine internal cubes into one.
    TO-DO: next step is to update internal part of the basic cube based on the recived internal cube.

--- src/pGas.cpp
+++ src/pGas.cpp
@@ -13,54 +13,37 @@ using namespace std;
 using namespace arma;
 
 int taskP; // p - processors count
-int taskN = 64; // n - number of cells
-int taskm = 8; // m - number of blocks
-// int taskN = 216;
-// int taskm = 27;
+// int taskN = 64; // n - number of cells
+int taskN = 216;
+int taskm; // m - number of blocks
 int taskK = 1; // K - number of parameters
 int taskt = 1; // t - shadow area size
 
-void extendCubeToShadowArea(cube &cube) {
-  int extendedSize = cube.n_slices + taskt;
-  cube.insert_slices(0, taskt);
-  cube.insert_slices(cube.n_slices, taskt);
-  cube.resize(extendedSize + taskt, extendedSize + taskt, extendedSize + taskt);
-  for (uword k = 0; k < cube.n_slices; k++) {
-      mat cubeMat = cube.slice(k);
-      cubeMat.shed_rows(extendedSize, extendedSize + taskt - 1);
-      cubeMat.shed_cols(extendedSize, extendedSize + taskt - 1);
-      cubeMat.insert_cols(0, taskt);
-      cubeMat.insert_rows(0, taskt);
-      cube.slice(k) = cubeMat;
-  }
-}
-
-double* splitIntoSubcubes (cube basicCube, int numParts, int subCubeSize) {
-
+double* splitIntoSubcubes (cube basicCube, int numParts, int internalCubeSide, int numPartsDimm) {
   double* subCubes;
-  int numPartsDimm = cbrt(numParts);
-  int side = subCubeSize;
-  subCubes = (double *) malloc(numParts*pow((side+taskt*2), 3)*sizeof(double));
+  subCubes = (double *) malloc(numParts*pow((internalCubeSide+taskt*2), 3)*sizeof(double));
+  int side = internalCubeSide/cbrt(numParts);
   int num=0;
   for (int i=0; i < numPartsDimm; i++)
     for (int j=0; j < numPartsDimm; j++)
       for (int k=0; k < numPartsDimm; k++) {
-        cube subCube = basicCube.subcube( 0 + i*subCubeSize, 0 + j*subCubeSize, 0 + k*subCubeSize, subCubeSize - 1  + i * subCubeSize + taskt*2, subCubeSize - 1  + j * subCubeSize + taskt*2, subCubeSize - 1  + k * subCubeSize + taskt*2);
+        cube subCube = basicCube.subcube( 0 + i*side, 0 + j*side, 0 + k*side, side - 1 + i * side + taskt*2, side - 1  + j * side + taskt*2, side - 1 + k * side + taskt*2);
         memcpy(subCubes + subCube.size() * num , subCube.begin(), subCube.size() * sizeof(double));
         num++;
   }
   return subCubes;
 }
 
-cube combineSubcubes(double* &arraySendCubes, int numParts, int subCubeSize, int cubeSize, int nodeCellsNum) {
-  int numPartsDimm = cbrt(numParts);
+cube combineSubcubes(double* &arrayCubes, int numParts, int numPartsDimm, int subCubeSide, int basicCubeSide, int subCubeNum) {
+
   vector<cube> subCubes;
+
   for (int i=0; i<numParts; i++) {
-    cube cube(arraySendCubes + i*nodeCellsNum, subCubeSize, subCubeSize, subCubeSize);
+    cube cube(arrayCubes + i*subCubeNum, subCubeSide, subCubeSide, subCubeSide);
     subCubes.push_back(cube);
   }
-  delete [] arraySendCubes;
-  arraySendCubes = NULL;
+  delete [] arrayCubes;
+  arrayCubes = NULL;
   cube newCube;
   for (int i=0; i < numPartsDimm; i++) {
     cube cubeY;
@@ -69,21 +52,21 @@ cube combineSubcubes(double* &arraySendCubes, int numParts, int subCubeSize, int
       for (int k=0; k < numPartsDimm; k++) {
         cubeZ = join_slices( cubeZ, subCubes[i * numPartsDimm * numPartsDimm + j * numPartsDimm + k]);
       }
-      cubeY.resize(subCubeSize, subCubeSize*(j+1), cubeSize);
+      cubeY.resize(subCubeSide, subCubeSide*(j+1), basicCubeSide);
       for (uword k = 0; k < cubeY.n_slices; k++) {
           mat cubeYMat = cubeY.slice(k);
-          cubeYMat.shed_cols(subCubeSize*j, subCubeSize*(j+1)-1);
+          cubeYMat.shed_cols(subCubeSide*j, subCubeSide*(j+1)-1);
           mat cubeZMat = cubeZ.slice(k);
           cubeYMat.insert_cols(cubeYMat.n_cols, cubeZMat);
           cubeY.slice(k) = cubeYMat;
       }
     }
-    newCube.resize(subCubeSize*(i+1), cubeSize, cubeSize);
+    newCube.resize(subCubeSide*(i+1), basicCubeSide, basicCubeSide);
     for (uword j = 0; j < newCube.n_slices; j++)
     {
         mat newCubeMat = newCube.slice(j);
         mat cubeYMat = cubeY.slice(j);
-        newCubeMat.shed_rows(subCubeSize*i, subCubeSize*(i+1)-1);
+        newCubeMat.shed_rows(subCubeSide*i, subCubeSide*(i+1)-1);
         newCubeMat.insert_rows(newCubeMat.n_rows, cubeYMat);
         newCube.slice(j) = newCubeMat;
     }
@@ -91,29 +74,58 @@ cube combineSubcubes(double* &arraySendCubes, int numParts, int subCubeSize, int
   return newCube;
 }
 
-void updateParametetsValue(cube &cube, int subCubeSize) {
+void updateParametetsValue(cube &cube, int nodeCubeSide) {
   int dimSize = cbrt(cube.size());
   for (int i=0+taskt; i<dimSize-taskt; i++)
     for (int j=0+taskt; j<dimSize-taskt; j++)
       for (int k=0+taskt; k<dimSize-taskt; k++) {
         for (int m=1; m<=taskt; m++) cube(i,j,k) += cube(i-m,j,k) + cube(i+m,j,k) + cube(i,j-m,k) + cube(i,j+m,k) + cube(i,j,k-m) + cube(i,j,k+m);
         cube(i,j,k) = cube(i,j,k)/(6*taskt);
+        // cube(i,j,k) = 0;
       }
 }
 
-cube cropCubeBack(cube &nodeCube, int subCubeSize) {
-  nodeCube.shed_slices(subCubeSize+1, subCubeSize+1+taskt-1);
+cube extractInternalPart(cube &nodeCube, int internalCubeSide) {
+
+  nodeCube.shed_slices(internalCubeSide+1, internalCubeSide+1+taskt-1);
   nodeCube.shed_slices(0, taskt-1);
-  cube cube(subCubeSize, subCubeSize, subCubeSize);
+  cube cube(internalCubeSide, internalCubeSide, internalCubeSide);
   for (uword i = 0; i < nodeCube.n_slices; i++) {
       mat nodeCubeMat = nodeCube.slice(i);
-      nodeCubeMat.shed_cols(subCubeSize+1, subCubeSize+1+taskt-1);
+      nodeCubeMat.shed_cols(internalCubeSide+1, internalCubeSide+1+taskt-1);
       nodeCubeMat.shed_cols(0, taskt-1);
-      nodeCubeMat.shed_rows(subCubeSize+1, subCubeSize+1+taskt-1);
+      nodeCubeMat.shed_rows(internalCubeSide+1, internalCubeSide+1+taskt-1);
       nodeCubeMat.shed_rows(0, taskt-1);
       cube.slice(i) = nodeCubeMat;
   }
   return cube;
+}
+
+void updateInternalPart(cube &basicCube, cube &internalCube, int internalCubeSide) {
+
+  // TO-DO !
+
+  // internalCube.resize(internalCubeSide + taskt*2, internalCubeSide + taskt*2, internalCubeSide + taskt*2);
+  // for (uword k = taskt; k < internalCube.n_slices - taskt; k++) {
+  //     mat internalCubeMat = internalCube.slice(k);
+  //     internalCubeMat.shed_rows(internalCubeSide, internalCubeSide + taskt*2 - 1);
+  //     internalCubeMat.shed_cols(internalCubeSide, internalCubeSide + taskt*2 - 1);
+  //     mat basicCubeMat = cube.slice(k);
+  //     internalCubeMat.insert_cols(0, taskt);
+  //     internalCubeMat.insert_rows(0, taskt);
+  //     internalCubeMat.insert_cols(0, taskt);
+  //     internalCubeMat.insert_rows(0, taskt);
+  //     cube.slice(k) = cubeMat;
+  // }
+  // for (int i=0; i<taskt; i++) {
+  //   mat basicCubeSlice = basicCube.slice(i);
+  //   internalCube.insert_slices(0+i, basicCubeSlice);
+  // }
+  // for (int i=internalCubeSide; i<internalCubeSide+taskt; i++) {
+  //   mat basicCubeSlice = basicCube.slice(i);
+  //   internalCube.insert_slices(internalCubeSide, basicCubeSlice);
+  // }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -122,6 +134,7 @@ int main(int argc, char *argv[]) {
   MPI_Comm comm = MPI_COMM_WORLD;
   MPI_Comm_rank(comm, &id);
   MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+  taskm = worldSize;
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
@@ -130,43 +143,56 @@ int main(int argc, char *argv[]) {
   // Initialize the random generator
   arma_rng::set_seed_random();
 
-  int cellsNum = taskN;
   int numParts = taskm;
-  int cubeSize = cbrt(cellsNum);
-  int numPartsDimm = cbrt(numParts);
-  int subCubeSize = cubeSize / numPartsDimm;
+  int numPartsDimm = cbrt(taskm);
 
-  cube extendedNodeCube(subCubeSize+taskt*2, subCubeSize+taskt*2, subCubeSize+taskt*2);
-  cube nodeCube(subCubeSize, subCubeSize, subCubeSize);
-  cube basicCube(cubeSize, cubeSize, cubeSize);
+  int basicCubeNum = taskN;
+  int basicCubeSide = cbrt(basicCubeNum);
+
+  int internalCubeSide = basicCubeSide - 2*taskt;
+  int internalCubeNum = pow(internalCubeSide, 3);
+
+  int internalSubCubeSide = (basicCubeSide - 2*taskt)/numPartsDimm;
+  int internalSubCubeNum = pow(internalSubCubeSide, 3);
+
+  int nodeCubeSide = (internalCubeSide/numPartsDimm) + 2*taskt;
+  int nodeCubeNum = pow(nodeCubeSide, 3);
+
+  cube nodeCube(nodeCubeSide, nodeCubeSide, nodeCubeSide);
+  cube basicCube(basicCubeSide, basicCubeSide, basicCubeSide);
   double* arraySendCubes;
   double* arrayRecvCubes;
   vector<cube> subCubes;
 
   if (id == 0) {
-    basicCube = randu<cube>(cubeSize, cubeSize, cubeSize);
-    extendCubeToShadowArea(basicCube);
-    arraySendCubes = splitIntoSubcubes(basicCube, numParts, subCubeSize);
+    // basicCube = randu<cube>(basicCubeSide, basicCubeSide, basicCubeSide);
+    int num = 0;
+    for (int i=0; i<basicCubeNum; i++) {
+      basicCube[i] = num;
+      num++;
+    }
+    arraySendCubes = splitIntoSubcubes(basicCube, numParts, internalCubeSide, numPartsDimm);
   }
-  MPI_Scatter( arraySendCubes, extendedNodeCube.size(), MPI_DOUBLE, extendedNodeCube.begin(), extendedNodeCube.size(), MPI_DOUBLE, 0, comm );
+  MPI_Scatter( arraySendCubes, nodeCubeNum, MPI_DOUBLE, nodeCube.begin(), nodeCubeNum, MPI_DOUBLE, 0, comm );
   if (id==0) {
-    arrayRecvCubes = (double *) malloc(pow(cubeSize, 3)*sizeof(double));
+    delete [] arraySendCubes;
+    arraySendCubes = NULL;
+    arrayRecvCubes = (double *) malloc(pow(internalCubeSide, 3)*sizeof(double));
   }
+  updateParametetsValue(nodeCube, nodeCubeNum);
 
-  updateParametetsValue(extendedNodeCube, cubeSize);
+  // for (int i=0; i<100000; i++) {
+  //   updateParametetsValue(nodeCube, nodeCubeNum);
+  // }
 
-  for (int i=0; i<100000; i++) {
-    updateParametetsValue(extendedNodeCube, cubeSize);
-  }
+  cube internalSubCube = extractInternalPart(nodeCube, internalSubCubeSide);
 
-  nodeCube = cropCubeBack(extendedNodeCube, subCubeSize);
-
-  MPI_Gather( nodeCube.begin(), nodeCube.size(), MPI_DOUBLE, arrayRecvCubes, nodeCube.size(), MPI_DOUBLE, 0, comm );
+  MPI_Gather( internalSubCube.begin(), internalSubCubeNum, MPI_DOUBLE, arrayRecvCubes, internalSubCubeNum, MPI_DOUBLE, 0, comm );
 
   if (id == 0) {
-    basicCube = randu<cube>(cubeSize, cubeSize, cubeSize);
-    basicCube = combineSubcubes(arrayRecvCubes, numParts, subCubeSize, cubeSize, nodeCube.size());
-    cout << basicCube << endl;
+    // basicCube = randu<cube>(basicCubeSide, basicCubeSide, basicCubeSide);
+    cube internalCube = combineSubcubes(arrayRecvCubes, numParts, numPartsDimm, internalSubCubeSide, internalCubeSide, internalSubCubeNum);
+    updateInternalPart(basicCube, internalCube, internalCubeSide);
   }
 
   MPI_Finalize();
